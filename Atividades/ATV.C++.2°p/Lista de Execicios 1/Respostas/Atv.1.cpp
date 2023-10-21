056d6c7358f3850f40ba84aa0223e9c32b3ef90f
vandersonamaral <vandersonamaral27@gmail.com>
2023-10-21 17:51:28 -0300

    Adicionando atv.2p

---
+++ Atividades/ATV.C++.2°p/Lista de Execicios 1/Respostas/Atv.1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+struct Endereco{
+	char logr[50];
+	int numero;
+	char cidade[50];
+	char uf[3];
+	char cep[10];
+};
+int main() {
+	Endereco a;
+	return 0;
+}
